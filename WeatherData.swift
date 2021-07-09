f30abde9735d04edd282d17da90247681a63e635
JoaoM4theus <j.matheusr@hotmail.com>
2021-07-09 12:00:32 -0300

    estudo de api. curso na udemy

---
+++ WeatherData.swift
@@ -0,0 +1,9 @@
+//
+//  weatherData.swift
+//  Clima
+//
+//  Created by Softbuilder Hibrido on 18/03/21.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+
+import Foundation
---
+++ WeatherManager.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by Softbuilder Hibrido on 17/03/21.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+
+import Foundation
